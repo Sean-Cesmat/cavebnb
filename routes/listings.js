2e618a6812a7d39bb5f566ea0f3cf56150dbf340
Daniel Heyward <danheyward@gmail.com>
2018-03-15 13:11:36 -0700

    added get route

---
+++ routes/listings.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Listing = require('../models/listing');
+
+
+router.get('/listings/:location', (req, res) => {
+  let location = ''
+  if (req.params.location === 'big-rock') {
+    location = 'Big Rock'
+  } else if (req.params.location === 'little-water') {
+    location = 'Little Water'
+  } else if (req.params.location === 'big-water') {
+    location = 'Big Water'
+  } else {
+    location = 'Many Tree'
+  }
+  Listing.find({ location }, function(err, location) {
+    if (err) {
+      console.log(`Got an error finding the listings: ${err}`)
+    } else {
+      console.log(location)
+      res.send(location)
+    }
+  })
+})
+
+module.exports = router;
